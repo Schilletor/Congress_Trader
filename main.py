6efbd3d050a117f8b110ce62346d4c6efd53fc34
Schilletor <96495943+Schilletor@users.noreply.github.com>
2022-05-06 01:32:13 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 zip_url = 'https://disclosures-clerk.house.gov/public_disc/financial-pdfs/2022FD.ZIP'
 pdf_url = 'https://disclosures-clerk.house.gov/public_disc/ptr-pdfs/2022/'
 
-pickone = 'Pelosi'
+pickone = input('Who is going to represent your wallet?')
 
 r = requests.get(zip_url)
 thezipfile = '2022.zip'
@@ -30,46 +30,45 @@ with open('2022FD.txt') as f:
             with open("C:/Users/dan/PycharmProjects/CongTrader/pdffolder/"f"{doc_id}.pdf", 'wb') as pdf_file:
                 pdf_file.write(r.content)
 
-# doc = fitz.open('pdffolder/'f"{doc_id}.pdf")
-doc = fitz.open('pdffolder/20020515.pdf')
+            doc = fitz.open('pdffolder/'f"{doc_id}.pdf")
 
-page = doc.load_page(page_id=0)
+            page = doc.load_page(page_id=0)
 
-data = page.get_text().split('\n')
+            data = page.get_text().split('\n')
 
-data = data
+            strainer = []
 
-strainer = []
+            for word in data:
+                starttag = '$200?'
+                endtag = '* For the complete list of asset type abbreviations, please visit https://fd.house.gov/reference/asset-type-codes.aspx.'
 
-for word in data:
-    starttag = '$200?'
-    endtag = 'initial public offerings'
-    if word == starttag:
-        # strainer.append(data[data.index(starttag)+1:data.index(endtag)+1])
-        strainer.append(endtag in (word.lower() for word in data))
-print(strainer)
-doc.close()
+                if word == starttag:
+                    strainer.append(data[data.index(starttag)+1:data.index(endtag)-2])
 
-sifter = []
+            doc.close()
 
-for i in strainer:
-    for j in i:
-        if len(j) > 3:
-            sifter.append(j)
+            sifter = []
 
-bad_words = ('FIlINg STATuS', 'DESCRIPTION')
-result=[]
-for i in sifter:
-    if i.startswith(bad_words) == False:
-        result.append(i)
+            for i in strainer:
+                for j in i:
+                    if len(j) > 3:
+                        sifter.append(j)
 
-print(result)
+            bad_words = ('FIlINg STATuS', 'DESCRIPTION')
+            result = []
+            for i in sifter:
+                if i.startswith(bad_words) == False:
+                    result.append(i)
+                else:
+                    pass
+
+            print(result)
 
 
-# mydir = r'C:\Users\Dan\PycharmProjects\CongTrader\pdffolder'
-#
-# for f in os.listdir(mydir):
-#     if not f.endswith(".pdf"):
-#         continue
+mydir = r'C:\Users\Dan\PycharmProjects\CongTrader\pdffolder'
 
-#     os.remove(os.path.join(mydir,f))
+for f in os.listdir(mydir):
+    if not f.endswith(".pdf"):
+        continue
+
+    os.remove(os.path.join(mydir,f))
